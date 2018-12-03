aee934cac7d907f74b82de855b8987c5671b9431
Edward <hikaru1992@126.com>
2018-12-03 18:08:51 +0800

    commit

--- miniprogram/pages/heros/heros.js
+++ miniprogram/pages/heros/heros.js
@@ -65,14 +65,52 @@ Page({
   onShareAppMessage: function () {
 
   },
-
-  //获取英灵列表
+  /**
+   * 获取英灵列表
+   */
   getHerosList: function(){
     // console.log('ss')
     const db = wx.cloud.database()
     // 查询当前用户所有的 counters
+    db.collection('heros').get({
+      success: res => {
+        this.setData({
+          queryResult: JSON.stringify(res.data, null, 2)
+        })
+        this.setData({
+          herosList: res.data
+        })
+        console.log('[数据库] [查询记录] 成功: ', res)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
+  /**
+   * 条件查询
+   */
+  getHerosListByRequire: function(e){
+    // console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    const db = wx.cloud.database()
+
+    let req = {}
+    let val = e.detail.value
+    for(let i in val){
+      if(val[i] !== ''){
+        let descriptor = Object.create(null);
+        descriptor.value = val[i]
+        Object.defineProperty(req, i, descriptor)
+      }
+    }
+    console.log('req', req)
+    
     db.collection('heros').where({
-      _openid: this.data.openid
+      
     }).get({
       success: res => {
         this.setData({
@@ -82,10 +120,6 @@ Page({
           herosList: res.data
         })
         console.log('[数据库] [查询记录] 成功: ', res)
-        // this.herosList = res.data
-        // console.log(this.queryResult)
-        // console.log(this.herosList)
-        
       },
       fail: err => {
         wx.showToast({
@@ -94,6 +128,19 @@ Page({
         })
         console.error('[数据库] [查询记录] 失败：', err)
       }
+    })
+  },
+  formReset: function () {
+    console.log('form发生了reset事件')
+  },
+   /**
+   * 跳转详情页面
+   */
+  getHeroDetail: function(e){
+    // console.log('gethd', e.currentTarget.dataset.clickeditem)
+    let name = e.currentTarget.dataset.clickeditem.name
+    wx.navigateTo({
+      url: '../heroDetail/heroDetail?name=' + name
     })
   }
 })

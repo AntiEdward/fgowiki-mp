215be640b65716ee8e393472c1523abf51c44fc6
Edward <hikaru1992@126.com>
2019-03-21 20:38:42 +0800

    commit

--- miniprogram/pages/craft/craft.js
+++ miniprogram/pages/craft/craft.js
@@ -5,14 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    craftList: [],
+    pageSkip: 0,  //当前页数
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //页面加载时请求list
+    this.getCraftListFirst()
   },
 
   /**
@@ -62,5 +64,82 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+    /**
+   * 翻页
+   */
+    getNuxtPage: function(){
+        // console.log('getNuxtPage')
+        this.data.pageSkip++
+        this.getCraftList(this.data.pageSkip)
+    },
+    /**
+     * 初次查询 - 第一页数据
+     */
+    getCraftListFirst(){
+        const db = wx.cloud.database()
+        db.collection('craft-list')
+        .orderBy('craft_id', 'asc')
+        .limit(20)
+        .get({
+            success: res => {
+                
+                let array = this.data.craftList
+                // console.log('getCraftListFirst', array)
+                //过滤br符号
+                for(let i in res.data){
+                    res.data[i].skill_describe = res.data[i].skill_describe.replace(/<br \/>/g, "\n")
+                    // console.log(res.data[i].skill_describe)
+                }
+                array = array.concat(res.data)
+                this.setData({
+                    craftList: array,
+                })
+                console.log('[数据库] [查询记录] 成功: ', res)
+            },
+            fail: err => {
+                wx.showToast({
+                    icon: 'none',
+                    title: '查询记录失败'
+                })
+                console.error('[数据库] [查询记录] 失败：', err)
+            }
+        })
+        
+    },
+
+    /**
+     * 分页查询
+     */
+    getCraftList: function(page){
+        const db = wx.cloud.database()
+        db.collection('craft-list')
+        .orderBy('craft_id', 'asc')
+        .skip(page * 20)
+        .limit(20)
+        .get({
+            success: res => {
+                // console.log('getAllHerosList')
+                let array = this.data.craftList
+                //过滤br符号
+                for(let i in res.data){
+                    res.data[i].skill_describe = res.data[i].skill_describe.replace(/<br \/>/g, "\n")
+                    
+                    // console.log(res.data[i].skill_describe)
+                }
+                array = array.concat(res.data)
+                this.setData({
+                    craftList: array,
+                })
+                console.log('[数据库] [查询记录] 成功: ', res)
+            },
+            fail: err => {
+                wx.showToast({
+                    icon: 'none',
+                    title: '查询记录失败'
+                })
+                console.error('[数据库] [查询记录] 失败：', err)
+            }
+        })
+    }
 })
